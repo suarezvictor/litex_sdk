fc8299d860d1a4bb991dd8d648be83b360a6f2a7
Victor Suarez Rovere <suarezvictor@gmail.com>
2022-06-28 20:56:30 -0300

    added FAST_CODE and FAST_DATA macros for selecting linker sections, and minor fixes

---
+++ litex.h
@@ -0,0 +1,15 @@
+#ifndef _LITEX_H_
+#define _LITEX_H_
+
+#include <generated/soc.h>
+
+#ifdef __GNUC__
+#define FAST_DATA __attribute__ ((section (".fast_data")))
+#define FAST_CODE __attribute__ ((section (".fast_text")))
+#else
+#define FAST_DATA
+#define FAST_CODE
+#error You need a compiler that supports assigning code and data to specific linker sections
+#endif
+
+#endif
